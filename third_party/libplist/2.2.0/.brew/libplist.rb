591d74a861360538d263d9ae124ab29cf3f89d8c
Jon Gabilondo <jon.gabilondo.angulo@gmail.com>
2022-07-25 18:19:45 +0300

    Compile libplist as static lib.

---
+++ third_party/libplist/2.2.0/.brew/libplist.rb
@@ -0,0 +1,51 @@
+class Libplist < Formula
+  desc "Library for Apple Binary- and XML-Property Lists"
+  homepage "https://www.libimobiledevice.org/"
+  url "https://github.com/libimobiledevice/libplist/archive/2.2.0.tar.gz"
+  sha256 "7e654bdd5d8b96f03240227ed09057377f06ebad08e1c37d0cfa2abe6ba0cee2"
+  license "LGPL-2.1"
+
+  head do
+    url "https://git.sukimashita.com/libplist.git"
+  end
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+
+  def install
+    ENV.deparallelize
+
+    args = %W[
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --prefix=#{prefix}
+      --without-cython
+    ]
+
+    system "./autogen.sh", *args
+    system "make"
+    system "make", "install", "PYTHON_LDFLAGS=-undefined dynamic_lookup"
+  end
+
+  test do
+    (testpath/"test.plist").write <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>test</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>/bin/echo</string>
+        </array>
+      </dict>
+      </plist>
+    EOS
+    system bin/"plistutil", "-i", "test.plist", "-o", "test_binary.plist"
+    assert_predicate testpath/"test_binary.plist", :exist?,
+                     "Failed to create converted plist!"
+  end
+end
